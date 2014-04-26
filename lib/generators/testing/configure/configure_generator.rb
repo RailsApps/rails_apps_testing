2d6d01e3955bb943cf23efdc7401a89856a19ab0
Daniel Kehoe <daniel@danielkehoe.com>
2014-04-26 10:48:47 -0700

    initial implementation

---
+++ lib/generators/testing/configure/configure_generator.rb
@@ -0,0 +1,34 @@
+require 'rails/generators'
+
+module Testing
+  module Generators
+    class ConfigureGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+      argument :framework_name, :type => :string, :default => "rspec"
+
+      desc "Sets up a testing framework."
+
+      attr_reader :app_name
+
+      def configure_framework
+        case framework_name
+          when 'rspec'
+            run 'rm -rf test/' # Removing test folder (not needed for RSpec)
+            generate 'rspec:install'
+            inject_into_file '.rspec', "--format documentation\n", :after => "--color\n"
+            tweaks = File.read(find_in_source_paths('application.rb'))
+            inject_into_file 'config/application.rb', tweaks + "\n", :after => "Rails::Application\n"
+            gsub_file 'spec/spec_helper.rb', /check_pending/, 'maintain_test_schema'
+            copy_file 'capybara.rb', 'spec/support/capybara.rb'
+            gsub_file 'spec/spec_helper.rb', /config.use_transactional_fixtures = true/, "config.use_transactional_fixtures = false"
+            copy_file 'database_cleaner.rb', 'spec/support/database_cleaner.rb'
+            copy_file 'factory_girl.rb', 'spec/support/factory_girl.rb'
+            if File.exists?('config/initializers/devise.rb')
+              copy_file 'devise.rb', 'spec/support/devise.rb'
+            end
+        end
+      end
+
+    end
+  end
+end
---
+++ lib/generators/testing/configure/templates/application.rb
@@ -0,0 +1,11 @@
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
