441b1ced0fae0dc509183d0057a3189fb128cb73
Brian Salomaki <brian@salomaki.com>
2019-07-11 17:09:41 -0400

    Update users.rb

--- lib/generators/testing/configure/templates/spec/pundit/factories/users.rb
+++ lib/generators/testing/configure/templates/spec/pundit/factories/users.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name "Test User"
-    email "test@example.com"
-    password "please123"
+    name { "Test User" }
+    email { "test@example.com" }
+    password { "please123" }
 
     trait :admin do
-      role 'admin'
+      role { 'admin' }
     end
 
   end
