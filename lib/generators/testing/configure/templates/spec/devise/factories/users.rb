01bed0ec00a88cef29a6dccafb7d8bcddda52a14
Brian Salomaki <brian@salomaki.com>
2019-07-11 17:10:13 -0400

    Update users.rb

--- lib/generators/testing/configure/templates/spec/devise/factories/users.rb
+++ lib/generators/testing/configure/templates/spec/devise/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name "Test User"
-    email "test@example.com"
-    password "please123"
+    name { "Test User" }
+    email { "test@example.com" }
+    password { "please123" }
   end
 end
